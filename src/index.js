30bd54d81aa10fe0a4977e3f7aa3fb66278e200a
João Conrad <65290519+Conrad-C@users.noreply.github.com>
2022-05-02 10:21:36 -0300

    Starting dashboard css

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+})
+app.get('/dashboard', (req, res) => {
+    res.sendFile(__dirname + '/dashboard.html');
 })
 
 app.listen(PORT, () => {
