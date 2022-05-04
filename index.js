f5c5b73f763b2720bec1823eda8c07fdf21fea95
João Conrad <65290519+Conrage@users.noreply.github.com>
2022-05-04 18:28:38 -0300

    Fix relative path to dashboard

--- index.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("meubotao").onclick = function () {
-  window.open ("file:///D:/EDUARDO/Cursos/TERA/crmTeraProject-main/dashboard.html") 
-};
---
+++ js/index.js
@@ -0,0 +1,3 @@
+document.getElementById("meubotao").onclick = function () {
+  window.open ("./dashboard.html") 
+};
