b27a64fc6d622ae1d9c09a7a787ae914b728fb71
eduteixeirabhz <103132551+eduteixeirabhz@users.noreply.github.com>
2022-05-23 17:02:35 -0300

    Add files via upload
    Pagina de login repaginada, arquivos CSS limpos e algumas mudanças linkadas ao index.html.
    Página de cadastro criada e linkada à página de login pelo botão 'inscreva-se", porém ainda falta estilizar.

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("meubotao").onclick = function () {
+  window.open ("file:///D:/EDUARDO/Cursos/TERA/crmTeraProject-main/dashboard.html") 
+};
+document.getElementById("botaoinscreve").onclick = function () {
+  window.open ("file:///D:/EDUARDO/Cursos/TERA/crmTeraProject-main/inscricao.html") 
+};
---
+++ inscricao.js
@@ -0,0 +1,14 @@
+
+function formatar(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i);
+  
+  if (texto.substring(0,1) != saida){
+            documento.value += texto.substring(0,1);
+  }
+  
+}
+
+  
+ 
