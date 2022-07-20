27473a01922be5b500eb8a5319e47fd8272bca0a
João Victor Conrad <joao.conrad@deconve.com>
2022-07-20 16:53:23 -0300

    Add react fix folders

--- index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("meubotao").onclick = function () {
-  window.open ("file:///D:/EDUARDO/Cursos/TERA/crmTeraProject-main/dashboard.html") 
-};
-document.getElementById("botaoinscreve").onclick = function () {
-  window.open ("file:///D:/EDUARDO/Cursos/TERA/crmTeraProject-main/inscricao.html") 
-};
